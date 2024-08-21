133ddb7acf491509f608670fbcdcffdebaa956f0
Tomer Salakoff <tomer@om2.com>
2024-08-21 15:36:51 +0300

    Remove cloud block for successful init

--- terraform-worspace.tf
+++ terraform-worspace.tf
@@ -1,11 +1,11 @@
-terraform {
-  cloud {
-    organization = "OM2Phoenix"
-    hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
+# terraform {
+#   cloud {
+#     organization = "OM2Phoenix"
+#     hostname     = "app.terraform.io" # Optional; defaults to app.terraform.io
 
-    workspaces {
-      project = "om2-autotest"
-      name    = "terraform-autotest-vpc-requester"
-    }
-  }
-}
+#     workspaces {
+#       project = "om2-autotest"
+#       name    = "terraform-autotest-vpc-requester"
+#     }
+#   }
+# }
