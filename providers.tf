19509b1c990282fdb7c8a7c6ef076f7a81adf262
Max Perov <maxim.perov@jivygroup.com>
2025-10-23 14:50:55 +0300

    adds configuration_aliases for peovider

--- providers.tf
+++ providers.tf
@@ -2,8 +2,9 @@ terraform {
   required_version = ">=1.13.0, <1.14.0"
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">=6.13.0"
+      source                = "hashicorp/aws"
+      version               = ">=6.13.0"
+      configuration_aliases = [aws.accepter]
     }
   }
 }
